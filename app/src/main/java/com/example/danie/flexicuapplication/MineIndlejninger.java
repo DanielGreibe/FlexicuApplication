e853f02f3e5659d8ab73959a37d6f1a0cf9f650c
Daniel Greibe <danielgreibe@gmail.com>
2018-10-25 17:41:01 +0200

    Oprettet 8 grundlæggende skærmbilleder som skal være færdig til første iteration.

---
+++ app/src/main/java/com/example/danie/flexicuapplication/MineIndlejninger.java
@@ -0,0 +1,15 @@
+package com.example.danie.flexicuapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MineIndlejninger extends AppCompatActivity
+    {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+        {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine_indlejninger);
+        }
+    }
---
+++ app/src/main/java/com/example/danie/flexicuapplication/MineUdlejninger.java
@@ -0,0 +1,15 @@
+package com.example.danie.flexicuapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MineUdlejninger extends AppCompatActivity
+    {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+        {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine_udlejninger);
+        }
+    }
