5724b12583d9943b44f7ac8237fd2ff0a1bcf81a
Daniel Greibe <danielgreibe@gmail.com>
2018-10-28 17:16:36 +0100

    Changed backgruodn color of Login to Green. Changed text in the views at runtime to test.

--- app/src/main/java/com/example/danie/flexicuapplication/Login.java
+++ app/src/main/java/com/example/danie/flexicuapplication/Login.java
@@ -1,15 +1,54 @@
 package com.example.danie.flexicuapplication;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Login extends AppCompatActivity
+import static java.security.AccessController.getContext;
+
+public class Login extends AppCompatActivity implements View.OnClickListener
     {
-
+    TextView LoginLater;
+    EditText UsernameView;
+    EditText PasswordView;
+    Button Login;
+    ConstraintLayout LoginLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState)
         {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        LoginLater = findViewById(R.id.LoginLaterView);
+        UsernameView = findViewById(R.id.UsernameView);
+        PasswordView = findViewById(R.id.PasswordView);
+        Login = findViewById(R.id.LoginButton);
+        LoginLayout = findViewById(R.id.LoginLayout);
+
+
+
+        LoginLater.setOnClickListener(this);
+
+
+        UsernameView.setText("Runtime Changed Text");
+        PasswordView.setText("Runtime Changed Text");
+        Login.setText("Runtime Changed Text");
+        LoginLater.setText("Changed Text on Login Later button");
+
+        }
+
+    @Override
+    public void onClick(View v)
+        {
+        if(v == LoginLater)
+            {
+            Intent Navigation = new Intent(this, Navigation.class);
+            startActivity(Navigation);
+            }
         }
     }
