9493989052407e6ed07ce31e8092dbd013c205c2
Daniel Greibe <danielgreibe@gmail.com>
2018-10-31 15:39:52 +0100

    Insert ConstraintLayout in TestActivity programatically and add to activity.

--- app/src/main/java/com/example/danie/flexicuapplication/TestActivity.java
+++ app/src/main/java/com/example/danie/flexicuapplication/TestActivity.java
@@ -1,15 +1,19 @@
 package com.example.danie.flexicuapplication;
 
+import android.annotation.SuppressLint;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import static android.graphics.Color.GREEN;
+
 public class TestActivity extends AppCompatActivity
     {
         ConstraintLayout mainLayout;
         TextView mainText;
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState)
         {
@@ -17,10 +21,20 @@ public class TestActivity extends AppCompatActivity
         setContentView(R.layout.activity_test);
 
         mainLayout = findViewById(R.id.mainLayoutTestPage);
+
         mainText = findViewById(R.id.textView2);
 
-        Button myButton = new Button(this);
 
+        ConstraintLayout generatedLayout = new ConstraintLayout(this);
+        generatedLayout.setMaxWidth(400);
+        generatedLayout.setMaxHeight(200);
+        generatedLayout.setBackgroundColor(GREEN);
+
+        Button myButton = new Button(this);
+        myButton.setText("TestButton");
+
+
+        mainLayout.addView(generatedLayout);
 
         mainLayout.addView(myButton);
 
