d187a231372d589a03291c0ca1c2fd3243250dd0
futte111 <ntthyssen@gmail.com>
2018-10-31 14:57:51 +0100

    plain udlej

--- app/src/main/java/com/example/danie/flexicuapplication/TestActivity.java
+++ app/src/main/java/com/example/danie/flexicuapplication/TestActivity.java
@@ -6,10 +6,13 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
+
 public class TestActivity extends AppCompatActivity
     {
         ConstraintLayout mainLayout;
         TextView mainText;
+        int numberOfButtons = 10;
     @Override
     protected void onCreate(Bundle savedInstanceState)
         {
@@ -20,12 +23,7 @@ public class TestActivity extends AppCompatActivity
         mainText = findViewById(R.id.textView2);
 
         Button myButton = new Button(this);
-
-
         mainLayout.addView(myButton);
 
 
-
-
-        }
     }
