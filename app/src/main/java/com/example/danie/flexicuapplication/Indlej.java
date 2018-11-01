a0da9e26a8d6f1b01eb2224d50d663543a9e09c1
Hoegsgaard <mathiashoegsgaard@gmail.com>
2018-11-01 12:56:52 +0100

    Navigations tekst

--- app/src/main/java/com/example/danie/flexicuapplication/Indlej.java
+++ app/src/main/java/com/example/danie/flexicuapplication/Indlej.java
@@ -2,14 +2,32 @@ package com.example.danie.flexicuapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-public class Indlej extends AppCompatActivity
-    {
-//hey
+public class Indlej extends AppCompatActivity implements View.OnClickListener {
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-        {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_indlej);
+
+        LinearLayout scroller = findViewById(R.id.linearLayout);
+        ImageView filterMenu = findViewById(R.id.filterMenu);
+
+        filterMenu.setOnClickListener(this);
+
+
         }
+    public void createNew(){
+        LinearLayout LL = new LinearLayout(this);
+      
+
     }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
