2a72e18a2c11bfa68f20003d4a4101191dcfe8e4
futte111 <ntthyssen@gmail.com>
2018-11-01 16:50:00 +0100

    now cardviews can get added to a scrollview with text and image on buttonclick

--- app/src/main/java/com/example/danie/flexicuapplication/Udlej.java
+++ app/src/main/java/com/example/danie/flexicuapplication/Udlej.java
@@ -1,24 +1,65 @@
 package com.example.danie.flexicuapplication;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.support.v7.widget.CardView;
+import android.util.TypedValue;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Udlej extends AppCompatActivity
     {
-
+        private Context mContext;
+        private ConstraintLayout constLayout;
+        private ConstraintLayout constCardLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState)
         {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_udlej);
+        mContext = getApplicationContext();
         LinearLayout myContainer = findViewById(R.id.scrollLayoutUdlej);
-        LinearLayout myTestContainer = findViewById(R.id.scrollLayoutUdlej);
+        constLayout = findViewById(R.id.opretMedarbejder);
 
-        Button myButton = new Button(this);
-       // myContainer.addView(myTestContainer);
-        myTestContainer.addView(myButton);
+        constLayout.setOnClickListener((view) ->{
+            CardView card = new CardView(mContext);
+            card.setLayoutParams(getLinearLayout());
+            card.setRadius(15);
+            card.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
+            card.setCardElevation(9);
+
+            ImageView imgView = new ImageView(this);
+            imgView.setImageResource(R.drawable.download);
+
+            card.addView(addTextView(new TextView(mContext)));
+            card.addView(imgView);
+            myContainer.addView(card);
+
+        });
+
         }
+        public TextView addTextView(TextView TV){
+            TV.setLayoutParams(getLinearLayout());
+            TV.setText("Thomas - snedker");
+            TV.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12);
+            TV.setTextColor(Color.BLACK);
+
+            return TV;
+        }
+
+        public ViewGroup.LayoutParams getLinearLayout(){
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+
+            return  params;
+        }
+
     }
