6c0885b1887c273d3c9e3d68dd97e830cf9d757d
Hoegsgaard <mathiashoegsgaard@gmail.com>
2018-12-05 14:24:22 +0100

    Ret GUI til efter samtale med Henrik(Flexicu)

--- app/src/main/java/com/example/danie/flexicuapplication/CrudEmploye.java
+++ app/src/main/java/com/example/danie/flexicuapplication/CrudEmploye.java
@@ -11,9 +11,7 @@ public class CrudEmploye {
     int dist;
     boolean open;
     int id;
-
     int pic;
-    List<CrudEmploye> personer = new ArrayList<CrudEmploye>();
 
 
     public CrudEmploye(String name, String job, double rank, double pay, int dist, int id, int pic){
@@ -25,36 +23,29 @@ public class CrudEmploye {
         this.open = false;
         this.id = id;
         this.pic = pic;
-        personer.add(this);
     }
     //Getter
     public String getName() {
         return name;
     }
-
     public String getJob() {
         return job;
     }
-
     public double getRank() {
         return rank;
     }
-
     public double getPay() {
         return pay;
     }
-
     public int getDist() {
         return dist;
     }
-
     public boolean getOpen() {
         return open;
     }
     public boolean isOpen() {
         return open;
     }
-
     public int getId() {
         return id;
     }
@@ -66,23 +57,18 @@ public class CrudEmploye {
     public void setName(String name) {
         this.name = name;
     }
-
     public void setJob(String job) {
         this.job = job;
     }
-
     public void setRank(double rank) {
         this.rank = rank;
     }
-
     public void setPay(double pay) {
         this.pay = pay;
     }
-
     public void setDist(int dist) {
         this.dist = dist;
     }
-
     public void setOpen(boolean open) {
         this.open = open;
     }
