2faf4b9006007d73ebaec77b7372e2e92b498db4
Daniel Greibe <danielgreibe@gmail.com>
2018-12-05 15:37:40 +0100

    Ændret tekst fra FinishEmployuee fra engelsk til dansk

--- app/src/main/java/com/example/danie/flexicuapplication/CreateEmployeeFinish.java
+++ app/src/main/java/com/example/danie/flexicuapplication/CreateEmployeeFinish.java
@@ -47,11 +47,11 @@ public class CreateEmployeeFinish extends AppCompatActivity implements View.OnCl
         beskrivelse = previousIntent.getStringExtra("DescriptionOfEmployee");
 
 
-        textViewName.setText("Name of Employee: " + name);
-        textViewYear.setText("Birthyear of Employee: " + year);
-        textViewErhverv.setText("Erhverv of Employee : " + erhverv);
-        textViewPostcode.setText("Postcode of Employee: " + postcode);
-        textViewDescription.setText("Description of Employee: " + beskrivelse);
+        textViewName.setText("Navn: " + name);
+        textViewYear.setText("Fødselsår: " + year);
+        textViewErhverv.setText("Erhverv: " + erhverv);
+        textViewPostcode.setText("Postnummer: " + postcode);
+        textViewDescription.setText("Beskrivelse: " + beskrivelse);
     }
 
     @Override
