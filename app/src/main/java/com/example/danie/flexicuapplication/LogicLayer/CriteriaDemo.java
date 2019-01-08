de3a60c77e6f3166fe0d1ccb16223ac6f199a8ef
futte111 <ntthyssen@gmail.com>
2019-01-08 11:17:54 +0100

    gradle sync

--- app/src/main/java/com/example/danie/flexicuapplication/LogicLayer/CriteriaDemo.java
+++ app/src/main/java/com/example/danie/flexicuapplication/LogicLayer/CriteriaDemo.java
@@ -6,16 +6,20 @@ import java.util.List;
 public class CriteriaDemo {
     public void start() {
         List<CrudEmployee> people = new ArrayList<CrudEmployee>();
-        people.add(new CrudEmployee.EmployeBuilder("john").pay(201).job("mur").builder());
+        people.add(new CrudEmployee.EmployeBuilder("john").pay(201).job("mur").dist(70).builder());
         //people.add(new CrudEmployee.EmployeBuilder("fut").pay(199).builder());
-        people.add(new CrudEmployee.EmployeBuilder("bob").pay(202).job("elektro").builder());
-
+        people.add(new CrudEmployee.EmployeBuilder("bob").pay(202).job("elektro").dist(20).builder());
+        people.add(new CrudEmployee.EmployeBuilder("julemanden").pay(150).job("væg").dist(10).builder());
         CriteriaInterface pay = new CriteriaPay();
         CriteriaInterface profession = new CriteriaProfession();
+        CriteriaInterface distance = new CriteriaDistance();
+
         System.out.println("løn-------");
         printPeeps(pay.meetCriteria(people));
         System.out.println("job---------");
         printPeeps(profession.meetCriteria(people));
+        System.out.println(("distance-----------------"));
+        printPeeps(distance.meetCriteria(people));
     }
 
 
