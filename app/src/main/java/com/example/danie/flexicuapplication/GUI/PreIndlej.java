3f735ebaee029dd0f61949c2f2787fd4662f0292
Daniel Greibe <danielgreibe@gmail.com>
2019-01-11 10:53:05 +0100

    Added Address to GlobalVariables

--- app/src/main/java/com/example/danie/flexicuapplication/GUI/PreIndlej.java
+++ app/src/main/java/com/example/danie/flexicuapplication/GUI/PreIndlej.java
@@ -3,6 +3,7 @@ package com.example.danie.flexicuapplication.GUI;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -14,8 +15,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.danie.flexicuapplication.LogicLayer.GlobalVariables;
 import com.example.danie.flexicuapplication.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -28,7 +33,9 @@ import java.util.Locale;
 public class PreIndlej extends AppCompatActivity implements View.OnClickListener
     {
     TextView textViewDescription;
-    Button buttonGetLocation;
+    EditText editTextZipcode;
+    ImageButton buttonGetLocation;
+    Button buttonNextPage;
     private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
@@ -37,10 +44,15 @@ public class PreIndlej extends AppCompatActivity implements View.OnClickListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_indlej);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        editTextZipcode = findViewById(R.id.editTextZipcode);
 
+        buttonNextPage = findViewById(R.id.buttonNextPage);
+        buttonNextPage.setOnClickListener(this);
         textViewDescription = findViewById(R.id.textViewDescription);
         buttonGetLocation = findViewById(R.id.buttonGetLocation);
         buttonGetLocation.setOnClickListener(this);
+        buttonGetLocation.setImageResource(R.drawable.marker);
+        buttonGetLocation.setBackgroundDrawable(null);
 
 
         }
@@ -52,43 +64,65 @@ public class PreIndlej extends AppCompatActivity implements View.OnClickListener
             {
 
 
-
             //If you do not have permission to get GPS Data.
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED)
+                {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
 
-            } else {
-            Log.e("Permission", "You already had permission");
-            }
+                }
+            else
+                {
+                Log.e("Permission", "You already had permission");
+                }
 
             mFusedLocationClient.getLastLocation()
-                                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                                @Override
-                                public void onSuccess(Location location) {
-                                // Got last known location. In some rare situations this can be null.
-                                if (location != null) {
-                                Log.e("Test" , location.getLongitude() + "");
-                                Log.e("Test" , location.getLatitude() + "");
-
-                                Geocoder geocoder = new Geocoder(PreIndlej.this, Locale.getDefault());
-                                try
+                                .addOnSuccessListener(this, new OnSuccessListener<Location>()
                                     {
-                                    List<Address > addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-                                    Log.e("Test" , addresses.get(0).getFeatureName() + ", " + addresses.get(0).getLocality() + ", " + addresses.get(0).getAdminArea() + ", " + addresses.get(0).getCountryName());
-                                    } catch (IOException e)
-                                    {
-                                    e.printStackTrace();
-                                    }
-                                }
-                                }
-                                });
+                                    @Override
+                                    public void onSuccess(Location location)
+                                        {
+                                        // Got last known location. In some rare situations this can be null.
+                                        if (location != null)
+                                            {
+                                            /*
+                                            Log.e("Test", location.getLongitude() + "");
+                                            Log.e("Test", location.getLatitude() + "");
+                                            */
+                                            Geocoder geocoder = new Geocoder(PreIndlej.this, Locale.getDefault());
+                                            try
+                                                {
+                                                List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                                                ((GlobalVariables) PreIndlej.this.getApplication()).setUserAddress(addresses.get(0));
+                                                List<Address> addresses2 = geocoder.getFromLocationName("Kongens Lyngby" , 1);
+                                                Log.e("Address2" , "Latitude: " + addresses2.get(0).getLatitude());
+                                                Log.e("Address2" , "Longitude " + addresses2.get(0).getLongitude());
+                                                Log.e("Address1", addresses.get(0).getFeatureName() + ", " + addresses.get(0).getLocality() + ", " + addresses.get(0).getCountryName());
+                                                editTextZipcode.setText(addresses.get(0).getPostalCode() + "  -  " + addresses.get(0).getLocality());
 
-
-
-
+                                                Log.e("Addresse" , ((GlobalVariables) PreIndlej.this.getApplication()).getUserAddress().getPostalCode());
+                                                } catch (IOException e)
+                                                {
+                                                e.printStackTrace();
+                                                }
+                                            }
+                                        }
+                                    });
 
 
             }
+        else if ( v == buttonNextPage)
+            {
+            if ( editTextZipcode.getText().length() < 4)
+                {
+                Toast.makeText(this, "Tryk på knappen eller indtast et postnummer", Toast.LENGTH_SHORT).show();
+                }
+            else
+                {
+                Intent RentIn = new Intent(this, RentIn.class);
+                startActivity(RentIn);
+                }
+            }
+
         }
 
     }
