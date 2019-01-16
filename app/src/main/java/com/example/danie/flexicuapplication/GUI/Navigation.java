6d18d3d6d5368679001e569e29633e24a2e534f7
futte111 <ntthyssen@gmail.com>
2019-01-16 15:14:47 +0100

    new ui works

--- app/src/main/java/com/example/danie/flexicuapplication/GUI/Navigation.java
+++ app/src/main/java/com/example/danie/flexicuapplication/GUI/Navigation.java
@@ -12,8 +12,6 @@ import com.example.danie.flexicuapplication.R;
 public class Navigation extends AppCompatActivity implements View.OnClickListener
     {
 
-    Button buttonMineIndlejninger;
-    Button buttonMineUdlejninger;
     Button buttonIndlej;
     Button buttonUdlej;
 
@@ -23,15 +21,9 @@ public class Navigation extends AppCompatActivity implements View.OnClickListene
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_navigation);
-
-        buttonMineIndlejninger = findViewById(R.id.buttonMineIndlejninger);
-        buttonMineUdlejninger = findViewById(R.id.buttonMineUdlejninger);
         buttonIndlej = findViewById(R.id.buttonIndlej);
         buttonUdlej = findViewById(R.id.buttonUdlej);
 
-
-        buttonMineIndlejninger.setOnClickListener(this);
-        buttonMineUdlejninger.setOnClickListener(this);
         buttonIndlej.setOnClickListener(this);
         buttonUdlej.setOnClickListener(this);
         }
@@ -39,23 +31,8 @@ public class Navigation extends AppCompatActivity implements View.OnClickListene
     @Override
     public void onClick(View v)
         {
-            if ( v == buttonMineUdlejninger)
-                {
-                    //Opens the MineUdlejninger page
-                    Intent MineUdlejninger = new Intent(this, MyRentOuts.class);
-                    Bundle bndlanimation =
-                            ActivityOptions.makeCustomAnimation(getApplicationContext(), R.anim.anim_slide_in_left,R.anim.anim_slide_out_left).toBundle();
-                    startActivity(MineUdlejninger, bndlanimation);
-                }
-            else if ( v == buttonMineIndlejninger)
-                {
-                    //Opens the MineIndlejninger page
-                    Intent MineIndlejninger = new Intent(this, MyRentIns.class);
-                    Bundle bndlanimation =
-                            ActivityOptions.makeCustomAnimation(getApplicationContext(), R.anim.anim_slide_in_left,R.anim.anim_slide_out_left).toBundle();
-                    startActivity(MineIndlejninger, bndlanimation);
-                }
-            else if ( v == buttonIndlej)
+
+            if ( v == buttonIndlej)
                 {
                     //Opens the Indlej page
                     Intent Indlej = new Intent(this, RentIn.class);
