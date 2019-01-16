8a4551c97866b5d3ac776296758ef6aa16b9a8d5
Oliver <olivernielsen@msn.com>
2019-01-16 15:25:02 +0100

    Skrifttyper tilføjet Opret  bruger påbegyndt

---
+++ app/src/main/java/com/example/danie/flexicuapplication/GUI/CreateUser_cvr.java
@@ -0,0 +1,15 @@
+package com.example.danie.flexicuapplication.GUI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.danie.flexicuapplication.R;
+
+public class CreateUser_cvr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user_cvr);
+    }
+}
