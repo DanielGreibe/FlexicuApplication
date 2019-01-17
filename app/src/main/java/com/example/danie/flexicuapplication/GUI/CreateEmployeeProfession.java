f4adf202decd2cd1b8c593ebaee26364604cb46f
Hoegsgaard <mathiashoegsgaard@gmail.com>
2019-01-17 15:39:14 +0100

    Rettelser i navigation

--- app/src/main/java/com/example/danie/flexicuapplication/GUI/CreateEmployeeProfession.java
+++ app/src/main/java/com/example/danie/flexicuapplication/GUI/CreateEmployeeProfession.java
@@ -50,7 +50,7 @@ public class CreateEmployeeProfession extends AppCompatActivity implements View.
         items.add("VVS");
         items.add("Elektrkker");
             FirebaseDatabase database = FirebaseDatabase.getInstance();
-            DatabaseReference myRef2 = database.getReference(GlobalVariables.getFirebaseUser().getUid()+"/Medarbejdere");git
+            DatabaseReference myRef2 = database.getReference(GlobalVariables.getFirebaseUser().getUid()+"/Medarbejdere");
 
             ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
             editTextErhverv.setAdapter(adapter);
