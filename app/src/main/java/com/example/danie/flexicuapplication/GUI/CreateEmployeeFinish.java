77c9b24489fb78e12e45b8825916e324508c94e4
Oliver <olivernielsen@msn.com>
2019-01-23 22:21:31 +0100

    nullpointer crash bug fixet

--- app/src/main/java/com/example/danie/flexicuapplication/GUI/CreateEmployeeFinish.java
+++ app/src/main/java/com/example/danie/flexicuapplication/GUI/CreateEmployeeFinish.java
@@ -129,7 +129,7 @@ public class CreateEmployeeFinish extends AppCompatActivity implements View.OnCl
             CrudEmployee employee = new CrudEmployee.EmployeBuilder(name).job(profession).pay(Double.parseDouble(pay)).zipcode(zipcode).dist(distance).status("ikke udlejet").available("Home").owner(GlobalVariables.getFirebaseUser().getUid()).description(description).builder();
 
             //Upload image if standard image is selected or if custom image is selected!
-            if(((GlobalVariables) this.getApplication()).getTempEmployeeImage().equals("flexicu")){
+            if(imgData == null){
                 employee.setPic("flexicu"); //TODO ADD STANDARD IMG LINK
                 // Write a message to the database
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -241,7 +241,7 @@ public class CreateEmployeeFinish extends AppCompatActivity implements View.OnCl
         textViewProfession.setText(profession);
         textViewStatus.setText("ikke udlejet");
         textViewDescription.setText(description);
-     if (imgData.equals(null))
+     if (imgData == null)
             {
             //Get round image
             Glide.with(this).load(R.drawable.flexiculogocube).
